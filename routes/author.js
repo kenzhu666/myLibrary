916f2ace3faf5d4befe434b51d86c43557f2ce98
KenZhu <kenzhu@KenZhus-MacBook-Pro-3.local>
2020-07-29 13:50:15 -0400

    before file upload locally

--- routes/author.js
+++ routes/author.js
@@ -1,19 +1,42 @@
 const express = require('express');
-const author = express.Router();
+const router = express.Router();
+const Author = require('../models/author');
 
 // All authors route
-author.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    let searchAuthor = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchAuthor.name = new RegExp(req.query.name, 'i');
+    }
+    try {
+        const authors = await Author.find(searchAuthor);
+        res.render('authors/index', { authors: authors, searchAuthor: req.query });
+    } catch{
+        res.redirect('/');
+    }
 })
 
 // New author route
-author.get('/new', (req, res) => {
-    res.render('/authors/new');
+router.get('/new', (req, res) => {
+    // 在前端直接用author就可以
+    res.render('authors/new', { author: new Author() });
 })
 
 // Create author route
-author.post('/', (req, res) => {
-    res.send('create');
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save();
+        //res.redirect(`authors/${newAuthor.id}`);
+        res.redirect('/authors');
+    } catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        });
+    }
 })
 
-module.exports = author;
+module.exports = router;
--- server.js
+++ server.js
@@ -5,19 +5,23 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
-const authorRouter = require('./routes/author')
+const authorRouter = require('./routes/author');
+const bookRouter = require('./routes/book')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
